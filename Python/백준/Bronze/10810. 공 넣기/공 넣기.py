faca9f85670e5707ec888c3ea52264bc67e32ce4
Seoeun Hwang <sunny030921@gmail.com>
2026-01-29 20:49:25 +0900

    [Bronze III] Title: 공 넣기, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split(" "))
+
+arr = [0] * N
+
+for x in range(M):
+    i, j, k = map(int, input().split(" "))
+    
+    for y in range(i-1, j):
+        arr[y] = k
+
+for z in arr:
+    print(z, end=" ")
+    
+print()
