b444355ff083ba2394277033b42921c2be63ec5c
Seoeun Hwang <sunny030921@gmail.com>
2026-02-26 12:13:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 148.13 ms, Memory: 27.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)):
+        if len(answer) == 0 or answer[-1] != arr[i]:
+            answer.append(arr[i])
+        else:
+            continue
+    
+    return answer
