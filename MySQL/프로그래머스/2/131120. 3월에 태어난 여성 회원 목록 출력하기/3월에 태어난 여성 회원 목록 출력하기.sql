690efec308b9827f58d8a528e6777a660646b9c2
Seoeun Hwang <sunny030921@gmail.com>
2026-02-27 00:02:13 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, "%Y-%m-%d") from MEMBER_PROFILE
+where DATE_OF_BIRTH like "%-03-%" and GENDER like 'W' and TLNO is not null
+order by MEMBER_ID asc;
