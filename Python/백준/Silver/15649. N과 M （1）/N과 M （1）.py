bba9a8cac40908921dc75e4633d7263e36e16a27
Seoeun Hwang <sunny030921@gmail.com>
2026-03-30 11:03:45 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,30 +1,26 @@
 N, M = map(int, input().split())
 
+visited = [False] * N
+stack = []
+
 def out(result):
     for r in result:
         print(r, end=" ")
     print()
 
-def dfs(num, stack):
-    global N, M
-    
-    visited[num-1] = True
-    stack.append(num)
-    
+def dfs():
     if len(stack) == M:
         out(stack)
-        stack.pop()
-        visited[num-1] = False
         return
     
-    for i in range(1, N+1):
-        if not visited[i-1]:
-            dfs(i, stack)
-    
-    stack.pop()
-    visited[num-1] = False
+    for i in range(1, N + 1):
+        if not visited[i - 1]:
+            visited[i - 1] = True
+            stack.append(i)
+            
+            dfs()
+            
+            stack.pop()
+            visited[i - 1] = False
 
-for i in range(1, N+1):
-    stack = []
-    visited = [False] * N
-    dfs(i, stack)
+dfs()
