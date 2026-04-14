8bd83419aeffa31fab96f90207d261a10fadb82d
Seoeun Hwang <sunny030921@gmail.com>
2026-04-14 11:23:15 +0900

    [Gold IV] Title: 수 묶기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/1744. 수 묶기/수 묶기.py
@@ -0,0 +1,52 @@
+N = int(input())
+
+positive = []
+negative = []
+zero_cnt = 0
+one_cnt = 0
+
+for i in range(N):
+    num = int(input())
+    
+    if num == 0:
+        zero_cnt += 1
+    
+    elif num == 1:
+        one_cnt += 1
+    
+    elif num > 0 :
+        positive.append(num)
+    
+    elif num < 0 :
+        negative.append(num)
+
+sum = 0
+
+positive.sort()
+
+for i in range(len(positive)-1, -1, -2):
+    if len(positive) % 2 != 0 and i == 0:
+        sum += positive[i]
+    
+    else:
+        sum += positive[i] * positive[i-1]
+
+negative.sort()
+
+for i in range(0, len(negative), 2):
+    if len(negative) % 2 != 0 and i == len(negative) - 1:
+        if zero_cnt > 0:
+            sum += negative[i] * 0
+            zero_cnt -= 1
+        
+        else:
+            sum += negative[i]
+    
+    else:
+        sum += negative[i] * negative[i+1]
+
+while one_cnt > 0:
+    sum += 1
+    one_cnt -= 1
+
+print(sum)
