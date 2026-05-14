c71a15b69a62386b343e703deb726e2b9425aa43
Seoeun Hwang <sunny030921@gmail.com>
2026-05-14 11:29:51 +0900

    Time: 1905 ms (8.45%), Space: 19.8 MB (74.6%) - LeetHub

---
+++ LeetCode/Easy/0001-two-sum/Python3/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    answer.append(i)
+                    answer.append(j)
+        
+        return answer
