b778477f2d0e3649bdd968ad2116a666f3e54ec8
Seoeun Hwang <sunny030921@gmail.com>
2026-05-18 15:08:10 +0900

    Time: 6 ms (20.24%), Space: 51.4 MB (86.96%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/Java/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/Java/0200-number-of-islands.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public int numIslands(char[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
@@ -8,7 +10,7 @@ class Solution {
             for(int j=0; j<grid[0].length; j++) {
                 if(!visited[i][j] && grid[i][j] == '1') {
                     count += 1;
-                    dfs(i, j, visited, grid);
+                    bfs(i, j, visited, grid);
                 }
             }
         }
@@ -16,17 +18,27 @@ class Solution {
         return count;
     }
 
-    private void dfs(int x, int y, boolean[][] visited, char[][] grid) {
-        int[][] direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private void bfs(int x, int y, boolean[][] visited, char[][] grid) {
+        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        Deque<int[]> queue = new ArrayDeque<>();
+
         visited[x][y] = true;
+        queue.offer(new int[]{x,y});
 
-        for(int d[] : direction) {
-            int next_x = x + d[0];
-            int next_y = y + d[1];
+        while(queue.size() != 0) {
+            int[] temp = queue.poll();
+            int recent_x = temp[0];
+            int recent_y = temp[1];
 
-            if(next_x >= 0 && next_y >= 0 && next_x < visited.length && next_y < visited[0].length) {
-                if(!visited[next_x][next_y] && grid[next_x][next_y] == '1') {
-                    dfs(next_x, next_y, visited, grid);
+            for(int[] d : directions) {
+                int next_x = recent_x + d[0];
+                int next_y = recent_y + d[1];
+
+                if(next_x >= 0 && next_y >= 0 && next_x < visited.length && next_y < visited[0].length) {
+                    if(!visited[next_x][next_y] && grid[next_x][next_y] == '1') {
+                        visited[next_x][next_y] = true;
+                        queue.offer(new int[]{next_x, next_y});
+                    }
                 }
             }
         }
